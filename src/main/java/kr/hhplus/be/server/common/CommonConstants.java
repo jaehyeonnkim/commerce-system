790bdf37645cb01518d955218581f386ab998b85
jaehyeonkim <wogus6805@gmail.com>
2025-01-08 02:24:48 +0900

    chore: add constants

---
+++ src/main/java/kr/hhplus/be/server/common/CommonConstants.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.common;
+
+public class CommonConstants {
+    public static final int PAGESIZE = 10;
+    public static final String DEFAULT_SORT_CRITERIA = "createdAt";
+    public static final String DEFAULT_PAGE_NO = "0";
+}
