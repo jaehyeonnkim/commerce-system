22860fb674b695f9a6807e448ad768ab4139cf44
jaehyeonkim <wogus6805@gmail.com>
2025-01-08 11:45:13 +0900

    feat : 상품 단건 조회 추가

--- src/main/java/kr/hhplus/be/server/domain/product/infra/ProductCoreReaderRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/product/infra/ProductCoreReaderRepository.java
@@ -1,15 +1,27 @@
 package kr.hhplus.be.server.domain.product.infra;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import kr.hhplus.be.server.domain.product.model.Product;
 import kr.hhplus.be.server.domain.product.repository.ProductReaderRepository;
+import kr.hhplus.be.server.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductCoreReaderRepository  {
+@RequiredArgsConstructor
+public class ProductCoreReaderRepository implements ProductReaderRepository {
 
     private ProductReaderRepository productReaderRepository;
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public Product findById(Long productId) {
+        return em.find(Product.class, productId);
+    }
 
     public Page<Product> findAll(Pageable pageable) {
         return productReaderRepository.findAll(pageable);
--- src/main/java/kr/hhplus/be/server/domain/product/repository/ProductReaderRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/product/repository/ProductReaderRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductReaderRepository extends JpaRepository<Product, Long> {
+public interface ProductReaderRepository {
+
+    Product findById(Long productId);
 
     Page<Product> findAll(Pageable pageable);
 }
